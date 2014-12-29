96a606e9d05e6a83e73c92f434764d6b49bf7095
igoryen <igor.yentaltsev@gmail.com>
2014-12-29 15:31:43 -0500

    get push origin master

--- app/routes.php
+++ app/routes.php
@@ -1,8 +1,6 @@
 <?php
 
 Route::get('/', function(){
-  $users = DB::table('users')->get(); // 2
-  $user = DB::table('users')->find(1); // 3
-  //dd($user); // 5
-  return $user->username;
+  $users = DB::table('users')->where('username', '!=', 'igoryen')->get(); // 6
+  return $users;
 });
