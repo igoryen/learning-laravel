a8582b97bf196a2a7966c82ded0a479471cac4af
igoryen <igor.yentaltsev@gmail.com>
2014-12-29 15:27:58 -0500

    get username by user id

--- app/routes.php
+++ app/routes.php
@@ -3,5 +3,6 @@
 Route::get('/', function(){
   $users = DB::table('users')->get(); // 2
   $user = DB::table('users')->find(1); // 3
-  dd($user); // 5
+  //dd($user); // 5
+  return $user->username;
 });
